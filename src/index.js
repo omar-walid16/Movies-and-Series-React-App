8a9e14487e6d99c5796e24304fa6fbe8e556942f
OMAR WALID <omarwalid20099@gmail.com>
2025-09-27 03:29:29 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'jquery/dist/jquery.min.js';
 import './index.css';
-import { BrowserRouter , HashRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
